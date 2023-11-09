e39f0dadf9eb30a5c15eccf71c64d3613808643c
angeljoelsoto <149635631+angeljoelsoto@users.noreply.github.com>
2023-11-09 16:36:15 -0400

    tarea 1

---
+++ Tarea1/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int resp = calcularAreaCuadrado(10);
+        Console.WriteLine(resp);
+
+    }
+
+
+        static int calcularAreaCuadrado(int L){
+            int resultado = L * L;
+            return resultado;
+
+
+        }
+}
