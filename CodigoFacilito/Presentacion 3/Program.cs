5f72f33e5e44e0d40e6437694d8d765c81d05c15
angeljoelsoto <149635631+angeljoelsoto@users.noreply.github.com>
2023-11-09 16:42:06 -0400

    código facilito

---
+++ CodigoFacilito/Presentacion 3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Presentacion_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            decimal x = 12.2m;
+            bool bandera = false;
+            string cadena = string.Empty;
+            DateTime fecha = DateTime.MinValue;
+
+            i = Convert.ToInt32(bandera);
+
+            Console.WriteLine("El valor de i es: {0}", i);
+            Console.WriteLine("El valor de x es; {0:c}", x);
+            Console.WriteLine("El valor de bandera es: " + bandera.ToString());
+            Console.WriteLine("El valor de cadena es: " + cadena);
+            Console.WriteLine("El valor de fecha: " + fecha.ToShortDateString());   
+            Console.ReadKey();
+
+
+        
+
+        }
+    }
+}
---
+++ CodigoFacilito/Presentacion4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Presentacion4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    
+            
+            
+             /**************************************************
+              *Este programa es del video 5 del codigo facilito*
+              **************************************************/
+
+            int valor1 = 0, valor2 =0;
+            string cadena;
+            Console.WriteLine("Dame el primer valor: ");
+            cadena = Console.ReadLine();
+            valor1 = Convert.ToInt32(cadena);
+
+            Console.WriteLine("Dame el segundo valor: ");
+            cadena = Console.ReadLine();
+            valor2 = Convert.ToInt32(cadena);
+
+
+            if (valor1 <= valor2) 
+            {
+                if (valor1 == valor2)
+                {
+                    Console.WriteLine("El valor 1 es igual al valor 2");
+                }else
+                    Console.WriteLine("El valor 2 es mayor que el valor 1");
+            }else 
+              {
+                Console.WriteLine("El valor 1 es mayor al valor 2");   
+              }
+            Console.ReadKey();  
+
+        }
+    }
+}
