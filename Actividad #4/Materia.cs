e58117f181568d77b714c657bc57b8786f5959da
angeljoelsoto <149635631+angeljoelsoto@users.noreply.github.com>
2023-11-10 14:59:02 -0400

    avance actividad 4

---
+++ Actividad #4/Materia.cs
@@ -0,0 +1,15 @@
+public class Materia
+{
+    public string Grupo; 
+    public string nomeMateria; 
+    public string nomDocente;
+
+    public Materia()
+    {
+        Grupo = "no asignado";
+        nomeMateria = "Sin nombre de materia";
+        nomDocente = "Sin nombre del docente";
+    
+    }
+}
+
