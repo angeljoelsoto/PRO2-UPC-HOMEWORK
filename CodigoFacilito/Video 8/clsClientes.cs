6ded9521490b681bd36a2c9e4fb7aeb897249fc9
angeljoelsoto <149635631+angeljoelsoto@users.noreply.github.com>
2023-11-23 14:24:46 -0400

    avances

---
+++ CodigoFacilito/Video 8/clsClientes.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel;
+
+namespace video8
+{
+    public class clsClientes
+    {
+        private int _IdCliente;
+        public int IdClientes
+        {
+            get{ return _IdCliente;}
+            set{ _IdCliente = value;}
+        }
+
+        private string _Nombres;
+        public string Nombres
+        {
+            get{ return _Nombres;}
+            set{ _Nombres = value;}
+
+        }
+
+        private string _Apellidos;
+        public string Apellidos
+        {
+            get{}
+        }
+    }
+}
---
+++ Recursividad Exposición/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exposicion
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+           Console.WriteLine("ingrese el numero que quieres fcalcular su factorial");
+           int numero = Convert.ToInt32(Console.ReadLine());
+           int NumFactorial = Factorial (numero) ;
+
+           Console.WriteLine("el factorial de " + numero + " es igual a " + NumFactorial );
+
+        }
+        static int Factorial( int numero)
+        {
+            if(numero == 1)
+            {
+                return 1;
+            }else
+            {
+                return numero * Factorial(numero - 1);
+            }
+           
+         
+
+
+
+
+       
+        }
+    }
+}
